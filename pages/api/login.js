14166312fa8f9e95094c2e4dc064e5a712c7ce8a
Wertco-Bombas <suporte2@wertco.com.br>
2026-05-15 14:16:46 -0300

    Update login.js

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,5 +1,5 @@
-export default async function handler(req, res) {
+export default function handler(req, res) {
   return res.status(200).json({
-    body: req.body
+    debug: "VERSÃO DO GITHUB ATUALIZOU"
   });
 }
